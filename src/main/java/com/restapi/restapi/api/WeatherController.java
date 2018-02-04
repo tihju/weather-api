651d8fc7e4fb4cfa209bd4f45b2eb3cf72fd7e24
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-02-04 07:42:26 +0530

    readme

--- src/main/java/com/restapi/restapi/api/WeatherController.java
+++ src/main/java/com/restapi/restapi/api/WeatherController.java
@@ -1,4 +1,45 @@
 package com.restapi.restapi.api;
 
+import com.restapi.restapi.config.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class WeatherController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/byCity/{country}/{city}")
+    public @ResponseBody Object getWeatherByCity(@PathVariable String city, @PathVariable String country) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        ResponseEntity<Object> response = restTemplate.
+                getForEntity("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country +
+                        "&APPID=80730ceb55c51bebae397cfc46e03344",
+                Object.class);
+
+        return response;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/forecastByCity/{country}/{city}")
+    public @ResponseBody Object getForecastByCity(@PathVariable String city, @PathVariable String code) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        ResponseEntity<Object> response = restTemplate.
+                getForEntity("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + code +
+                                "&APPID=" + Configuration.API_KEY,
+                        Object.class);
+
+        return response;
+    }
+
 }
+
+
---
+++ src/main/java/com/restapi/restapi/config/Configuration.java
@@ -0,0 +1,5 @@
+package com.restapi.restapi.config;
+
+public class Configuration {
+    public static String API_KEY = "80730ceb55c51bebae397cfc46e03344";
+}
